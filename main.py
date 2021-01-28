ffa13b1b81202d800fef9b5eac714e10812a0bc9
TimPidashev <pidashev.tim@gmail.com>
2021-01-28 08:17:31 -0800

    STABLE RELEASE

--- main.py
+++ main.py
@@ -17,27 +17,33 @@ while still_grading:
     stars_achieved = input(colored("Number of stars achieved: ", "yellow"))
     avg_acc = input(colored("AVG typing accuracy: ", "cyan"))
 
-    #calulating percent
-    stars_achieved = float(stars_achieved)
-    stars_total = float(stars_total)
-    stars_percentage = '{0:.2f}'.format((stars_achieved / stars_total * 100))
-
-    #getting an avg between the stars grade and avg accuracy
-    def average(stars_percentage, avg_acc):
-        return (stars_percentage + avg_acc) / 2.0
-
-    avg = average(float(stars_percentage), float(avg_acc))
-
-    #literally just doing checks to get grade specific colors
-    if avg > 90.0:
-        print(colored(f"{avg}", "green"))
-
-    elif avg < 90.0 and avg > 50.0:
-        print(colored(f"{avg}", "yellow"))
-
-    elif avg < 50.0:
-        print(colored(f"{avg}", "red"))
+    if input != int:
+        print(colored("Error, you can't input letters or other characters in here!", "red"))
 
     else:
-        print(colored("A user input error has occured, restart program to continue grading!", "red"))
-        break
+
+        #calulating percent
+        stars_achieved = float(stars_achieved)
+        stars_total = float(stars_total)
+        stars_percentage = '{0:.2f}'.format((stars_achieved / stars_total * 100))
+
+        #getting an avg between the stars grade and avg accuracy
+        def average(stars_percentage, avg_acc):
+            return (stars_percentage + avg_acc) / 2.0
+
+        avg = average(float(stars_percentage), float(avg_acc))
+        round(avg, 2)
+
+        #literally just doing checks to get grade specific colors
+        if avg > 90.0:
+            print(colored(f"{avg}", "green"))
+
+        elif avg < 90.0 and avg > 50.0:
+            print(colored(f"{avg}", "yellow"))
+
+        elif avg < 50.0:
+            print(colored(f"{avg}", "red"))
+
+        else:
+            print(colored("A user input error has occured, restart program to continue grading!", "red"))
+            break
